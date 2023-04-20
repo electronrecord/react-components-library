6c25e2834144ea4e59cf38e5ad16c57c26147444
Cristian Gherghel <electronrecord@gmail.com>
2023-04-20 18:46:59 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,90 +3,31 @@ import {useEffect, useState} from "react"
 import './App.scss'
 
 function App () {
-  const [form, setForm] = useState({
+  const form = {
     name: {
+      label: 'Name',
       value: '',
-      rule: 'required|email'
+      rule: 'required|email',
+      placeholder: 'your name'
     },
     password: {
+      label: 'Password',
       value: '',
       type: 'password',
-      rule: 'required'
-    }
-  })
-
-  function handleInput ({key, value}) {
-    setForm(prev => ({
-      ...prev,
-      [key]: {
-        ...prev[key],
-        value,
-        invalid: prev[key].rule.split('|').reduce((acc, rule) => {
-          return acc || handleValidationRule({rule, value})
-        }, '')
-      }
-    }))
-  }
-
-  async function handleSubmit () {
-    Object.keys(form).forEach(key => {
-      validate(key)
-    })
-    const isValid = Object.keys(form).every(key => form[key].invalid === '')
-    if (isValid) {
-      const user = Object.keys(form).reduce((acc, key) => {
-        acc[key] = form[key].value
-        return acc
-      }, {})
-      console.log(`send to network`, user)
-      // dispatch(login(user))
-      // const response = await axios.post('/api/login', user)
+      rule: 'required',
+      placeholder: 'your password'
     }
   }
 
-  function handleValidationRule ({rule, value}) {
-    switch (rule) {
-      case 'required':
-        return value ? '' : rule
-      case 'email':
-        return /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/.test(value) ? '' : rule
-      case 'max':
-        return value.length > 6
-    }
-  }
-
-  function validate (key) {
-    setForm(prev => ({
-      ...prev,
-      [key]: {
-        ...prev[key],
-        invalid: prev[key].rule.split('|').reduce((acc, rule) => {
-          return acc || handleValidationRule({rule, value: prev[key].value})
-        }, '')
-      }
-    }))
+  async function handleSubmit (data) {
+    console.log(`send req`, data)
   }
 
   return (
     <div className="App">
-      <BaseForm onSubmit={handleSubmit}
-                buttonText='Login'>
-        <BaseInput name='name'
-                   label='Name'
-                   rule={form.name.rule}
-                   invalid={form.name.invalid}
-                   value={form.name.value}
-                   placeholder='your name'
-                   onInput={handleInput} />
-        <BaseInput name='password'
-                   label='Password'
-                   rule={form.password.rule}
-                   type={form.password.type}
-                   invalid={form.password.invalid}
-                   value={form.password.value}
-                   placeholder='your password'
-                   onInput={handleInput} />
-      </BaseForm>
+      <BaseForm data={form}
+                onSubmit={handleSubmit}
+                buttonText='Login' />
     </div>
   )
 }
--- src/components/BaseForm.jsx
+++ src/components/BaseForm.jsx
@@ -1,13 +1,74 @@
-export const BaseForm = function ({onSubmit, buttonText = '', children = []}) {
+import {BaseInput} from "./BaseInput.jsx"
+import {useState} from "react"
+
+export const BaseForm = function ({onSubmit, data = {}, buttonText = '', children = []}) {
+  const [form, setForm] = useState({...data})
 
   function handleSubmit (ev) {
     ev.preventDefault()
-    onSubmit()
+    Object.keys(form).forEach(key => {
+      validate(key)
+    })
+    const isValid = Object.keys(form).every(key => form[key].invalid === '')
+    if (isValid) {
+      const payload = Object.keys(form).reduce((acc, key) => {
+        acc[key] = form[key].value
+        return acc
+      }, {})
+      onSubmit(payload)
+    }
+  }
+
+  function handleInput ({key, value}) {
+    setForm(prev => ({
+      ...prev,
+      [key]: {
+        ...prev[key],
+        value,
+        invalid: prev[key].rule.split('|').reduce((acc, rule) => {
+          return acc || handleValidationRule({rule, value})
+        }, '')
+      }
+    }))
+  }
+
+  function validate (key) {
+    setForm(prev => ({
+      ...prev,
+      [key]: {
+        ...prev[key],
+        invalid: prev[key].rule.split('|').reduce((acc, rule) => {
+          return acc || handleValidationRule({rule, value: prev[key].value})
+        }, '')
+      }
+    }))
+  }
+
+  function handleValidationRule ({rule, value}) {
+    switch (rule) {
+      case 'required':
+        return value ? '' : rule
+      case 'email':
+        return /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/.test(value) ? '' : rule
+      case 'max':
+        return value.length > 6
+    }
   }
 
   return (
     <form onSubmit={handleSubmit}>
       {...children}
+      {children.length === 0 && Object.keys(data).map(key => (
+        <BaseInput key={key}
+                   name={key}
+                   label={form[key].label}
+                   rule={form[key].rule}
+                   type={form[key].type}
+                   invalid={form[key].invalid}
+                   value={form[key].value}
+                   placeholder={form[key].placeholder}
+                   onInput={handleInput}/>
+      ))}
       {buttonText && <button>{buttonText}</button>}
     </form>
   )
